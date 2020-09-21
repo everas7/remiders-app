c1c3b7062ff16980ea4160ad01826bc3cbb33d59
Enmanuel Veras <enmaveras07@gmail.com>
2020-09-21 17:38:11 -0400

    Adds validators

--- src/app/common/input-text/InputText.tsx
+++ src/app/common/input-text/InputText.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Form, Col, FormControlProps, } from 'react-bootstrap';
+import { Form, Col, FormControlProps } from 'react-bootstrap';
 
 interface Props extends FormControlProps {
   placeholder: string;
+  maxCharacters?: number;
 }
 
-export const InputText: React.FC<Props> = ({ ...props }) => {
-  return <Form.Control type="text" {...props}></Form.Control>;
+export const InputText: React.FC<Props> = ({
+  onChange,
+  maxCharacters,
+  ...props
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (maxCharacters && e.target.value?.length > maxCharacters) {
+      e.preventDefault();
+      return;
+    }
+    onChange && onChange(e);
+  };
+
+  return (
+    <Form.Control type="text" {...props} onChange={handleChange}></Form.Control>
+  );
 };
